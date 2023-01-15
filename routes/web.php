41fb39b6c09c8ffe16eebc5f3ff8fd6af25a4a48
João Marcos Pimenta Martins <joaomarcos-icm@hotmail.com>
2023-01-15 11:03:09 -0300

    feat: controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,43 +13,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', [ProductController::class, 'index'])->name('product.index');
+//Route::get('/product/{id?}', [ProductController::class, 'show'])->name('product.show');
+Route::resource('products', ProductController::class);//Froma de ja pegar as rotas padrões
 
 //Route::get('/company', function (){
 //   return view('company'); 
 //});
-Route::view('/company', 'company');//Forma simplificada de renderizar uma view
-
-Route::any('/any', function (){
-    return "Permite todo tipo de requisição http";
-});
-
-Route::match(['get', 'post'],'/match', function (){
-    return 'Permite apenas os acessos definidos';
-});
-
-Route::get('/product/{id}/{cat?}', function ($id, $cat = ''){//Interrogação e pq o parametro não e obrigatorio
-    return "Id do produto é: ".$id."<br>E sua categoria é: ".$cat;
-});
-
-//Route::get('/about', function (){
-//    return redirect('/company');//Redirecinamento de rota
+//Route::view('/company', 'company');//Forma simplificada de renderizar uma view
+//
+//Route::any('/any', function (){
+//    return "Permite todo tipo de requisição http";
 //});
-Route::redirect('/about', '/company');//Forma mais simplificada
-
-Route::get('/news', function (){
-    return view('news');
-})->name('noticias');//Declarando nome para rota e mesmo se a rota mudar ainda ira continuar redirecionando
-
-Route::get('new', function(){
-    return redirect()->route('noticias');//Fazer um redirect pelo nome da rota
-});
-
-Route::get('/redirect', function(){
-    return redirect()->route('admin.client');
-});
+//
+//Route::match(['get', 'post'],'/match', function (){
+//    return 'Permite apenas os acessos definidos';
+//});
+//
+//Route::get('/product/{id}/{cat?}', function ($id, $cat = ''){//Interrogação e pq o parametro não e obrigatorio
+//    return "Id do produto é: ".$id."<br>E sua categoria é: ".$cat;
+//});
+//
+////Route::get('/about', function (){
+////    return redirect('/company');//Redirecinamento de rota
+////});
+//Route::redirect('/about', '/company');//Forma mais simplificada
+//
+//Route::get('/news', function (){
+//    return view('news');
+//})->name('noticias');//Declarando nome para rota e mesmo se a rota mudar ainda ira continuar redirecionando
+//
+//Route::get('new', function(){
+//    return redirect()->route('noticias');//Fazer um redirect pelo nome da rota
+//});
+//
+//Route::get('/redirect', function(){
+//    return redirect()->route('admin.client');
+//});
 
 //Route::name('admin.')->group(function(){//Agrupamento de rotas por nome
 //    Route::get('admin/dashboard', function (){
@@ -80,19 +80,19 @@ Route::get('/redirect', function(){
 //    })->name('users');
 //});
 
-Route::group([
-        'prefix'=> 'admin',
-        'as' => 'admin.'//group
-    ], function (){//Agrupamento de rotas por nome e prefixo
-    Route::get('dashboard', function (){
-        return 'dasshboard';
-    })->name('dashboard');
-
-    Route::get('client', function (){
-        return 'client';
-    })->name('client');
-
-    Route::get('users', function (){
-        return 'users';
-    })->name('users');
-});
+//Route::group([
+//        'prefix'=> 'admin',
+//        'as' => 'admin.'//group
+//    ], function (){//Agrupamento de rotas por nome e prefixo
+//    Route::get('dashboard', function (){
+//        return 'dasshboard';
+//    })->name('dashboard');
+//
+//    Route::get('client', function (){
+//        return 'client';
+//    })->name('client');
+//
+//    Route::get('users', function (){
+//        return 'users';
+//    })->name('users');
+//});
